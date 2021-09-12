0d0b7afb929165e8970aaf1b01cdddbb20974438
Jefferson Mariano de Souza <jefferson.souza@synchro.com.br>
2021-09-12 10:36:43 -0300

    adjusting app for deployment

--- jdrive-api/src/index.js
+++ jdrive-api/src/index.js
@@ -1,4 +1,5 @@
 import https from 'https';
+import http from 'http';
 import fs from 'fs';
 import { logger } from './logger.js';
 import { Server } from 'socket.io';
@@ -11,8 +12,19 @@ const localHostSSL = {
   cert: fs.readFileSync('./certificates/cert.pem'),
 };
 
+const isProduction = process.env.NODE_ENV === 'production';
+process.env.USER = process.env.USER ?? 'system_user';
+
+const localHostSSL = {
+  key: fs.readFileSync('./certificates/key.pem'),
+  cert: fs.readFileSync('./certificates/cert.pem'),
+};
+
+const protocol = isProduction ? http : https;
+const sslConfig = isProduction ? {} : localHostSSL;
+
 const routes = new Routes();
-const server = https.createServer(localHostSSL, routes.handler.bind(routes));
+const server = protocol.createServer(sslConfig, routes.handler.bind(routes));
 
 const io = new Server(server, {
   cors: {
@@ -27,7 +39,8 @@ io.on('connection', (socket) => logger.info(`someone connected: ${socket.id}`));
 
 const startServer = () => {
   const { address, port } = server.address();
-  logger.info(`App running at https://${address}:${port}`);
+  const protocol = isProduction ? 'http' : 'https';
+  logger.info(`App running at ${protocol}://${address}:${port}`);
 };
 
 server.listen(PORT, startServer);
